5146ed65d4f9f33c1162671d88ee88592bc8fe6e
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2024-01-30 19:38:59 +0530

    updated css html & js

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Javascript vimallll")
+
+async function main(){
+
+    let a = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
